dc8701fec46544c71e088aa8b37903a01bcc2300
zq <xiaoyaozhe@qq.com>
2023-01-15 23:07:31 +0800

    first commit

---
+++ src/parser/goexpression_base_visitor.go
@@ -0,0 +1,76 @@
+// Code generated from java-escape by ANTLR 4.11.1. DO NOT EDIT.
+
+package parser // goexpression
+import "github.com/antlr/antlr4/runtime/Go/antlr/v4"
+
+type BasegoexpressionVisitor struct {
+	*antlr.BaseParseTreeVisitor
+}
+
+func (v *BasegoexpressionVisitor) VisitExpressionStmt(ctx *ExpressionStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitExpression(ctx *ExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitPrimaryExpr(ctx *PrimaryExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitOperand(ctx *OperandContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitOperandName(ctx *OperandNameContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitSlice_(ctx *Slice_Context) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitIndex(ctx *IndexContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitBasicLit(ctx *BasicLitContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitNil_lit(ctx *Nil_litContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitEn_bool(ctx *En_boolContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitFloat_lit(ctx *Float_litContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitInteger(ctx *IntegerContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitExpressionList(ctx *ExpressionListContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitArguments(ctx *ArgumentsContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitEos(ctx *EosContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitIdentifier(ctx *IdentifierContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BasegoexpressionVisitor) VisitString_(ctx *String_Context) interface{} {
+	return v.VisitChildren(ctx)
+}
---
+++ src/parser/goexpression_visitor.go
@@ -0,0 +1,60 @@
+// Code generated from java-escape by ANTLR 4.11.1. DO NOT EDIT.
+
+package parser // goexpression
+import "github.com/antlr/antlr4/runtime/Go/antlr/v4"
+
+// A complete Visitor for a parse tree produced by goexpressionParser.
+type goexpressionVisitor interface {
+	antlr.ParseTreeVisitor
+
+	// Visit a parse tree produced by goexpressionParser#expressionStmt.
+	VisitExpressionStmt(ctx *ExpressionStmtContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#expression.
+	VisitExpression(ctx *ExpressionContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#primaryExpr.
+	VisitPrimaryExpr(ctx *PrimaryExprContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#operand.
+	VisitOperand(ctx *OperandContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#operandName.
+	VisitOperandName(ctx *OperandNameContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#slice_.
+	VisitSlice_(ctx *Slice_Context) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#index.
+	VisitIndex(ctx *IndexContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#basicLit.
+	VisitBasicLit(ctx *BasicLitContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#nil_lit.
+	VisitNil_lit(ctx *Nil_litContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#en_bool.
+	VisitEn_bool(ctx *En_boolContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#float_lit.
+	VisitFloat_lit(ctx *Float_litContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#integer.
+	VisitInteger(ctx *IntegerContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#expressionList.
+	VisitExpressionList(ctx *ExpressionListContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#arguments.
+	VisitArguments(ctx *ArgumentsContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#eos.
+	VisitEos(ctx *EosContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#identifier.
+	VisitIdentifier(ctx *IdentifierContext) interface{}
+
+	// Visit a parse tree produced by goexpressionParser#string_.
+	VisitString_(ctx *String_Context) interface{}
+}
