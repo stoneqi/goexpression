f7294d5154dd82d82bcb3b04a00884f60eb7aa2f
zq <xiaoyaozhe@qq.com>
2023-03-29 00:09:10 +0800

    fix: 修复测试

--- interface_test.go
+++ interface_test.go
@@ -108,7 +108,7 @@ func TestGoExpression(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewGoExpression()
+			got := newGoExpression()
 			for key, value := range tt.expr {
 				err := got.AddExpr(key, value)
 				if err != nil {
